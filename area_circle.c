b924a7ddacc00329e5c8d5f9b6dcc88c9186dc19
Turyam <srikin@outlook.com>
2021-07-10 09:34:52 +0530

    Area of Circle and Triangle

--- area_circle.c
+++ area_circle.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 #include<math.h>
 
-
-int main(){
+float AreaofCircle(){
 	int r=0;
 	float pi=3.14;
 	float area;
@@ -14,7 +13,37 @@ int main(){
 	area = pi*r*r;
 	c= 2*pi*(r);
 	
-    printf("Radius of a Circle: %d\n",r);
+    	printf("Radius of a Circle: %d\n",r);
 	printf("Area of the circle is: %f\n",area);
-	printf("Circumference of the circle is: %f\n",c);		
+	printf("Circumference of the circle is: %f\n",c);
+}
+
+float AreaofTriangle(){
+	float height;
+	float base;
+	printf("Enter Height of the Triangle:");
+	scanf("%f",&height);
+	printf("\nEnter Base of the Triangle:");
+	scanf("%f",&base);
+	
+	float A= (height * base)/2;
+	printf("\nArea of  Traingle is: %f\n",A);
+}
+
+int main(){
+	int n;
+	printf("Press 1: Area and Circumference of Circle\nPress 2: Area of Triangle\n");
+	scanf("%d",&n);
+	switch (n) {
+	case 1:
+		AreaofCircle();
+		break;
+	case 2:
+		AreaofTriangle();
+		break;
+	default:
+		printf("Invalid Input");
+		break;
+	}
+	return 0;		
 }
