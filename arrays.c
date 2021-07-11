494d258a343c8489a099176e6a30fa7409b467de
Turyam <srikin@outlook.com>
2021-07-11 11:52:47 +0530

    Remove Duplicates Items In An Array

---
+++ arrays.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int count=0;
+
+int valueDetection(int arr[],int size){
+
+	for(int i =0; i<size; i++){
+		if(arr[i]==arr[i+1]){		//identifies the similar value
+			printf("Similar Element: %d at index %d\n",arr[i],i);	//prints the similar value
+			count++;		// to count how many times loop is executed
+			for(int j=i; j<=size;j++){
+				arr[j]=arr[j+1];		//delete the similar value
+			}
+			size--; // so that, null index at the end after above deletation dosen't count
+		}
+	}
+	printf("Count: %d\n",count);
+}
+
+int printArray(int arr[],int size){
+	printf("Array: ");
+	for(int i=0; i<size; i++){
+		printf("%d ",arr[i]);
+	}
+	printf("\n");
+}
+
+int main(){
+	int arr[100]={1,2,2,3,4,5,6,7,7};
+	int size=9;
+	
+	printf("Original ");
+	printArray(arr,size);
+	
+	valueDetection(arr,size);
+	size=size-count;			// array size after deletion of similar values
+	
+	printf("Modified ");
+	printArray(arr,size);
+	
+	return 0;
+}
